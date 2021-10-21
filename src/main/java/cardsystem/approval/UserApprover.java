40a8cb05ed26ee8ce352eb9e9fac291f74b4aa52
Nathan Quirke <49065040+NathanQuirke@users.noreply.github.com>
2021-10-21 16:16:49 +0100

    Initial approval implementation

--- src/main/java/cardsystem/approval/UserApprover.java
+++ src/main/java/cardsystem/approval/UserApprover.java
@@ -1,4 +1,37 @@
 package cardsystem.approval;
 
 public interface UserApprover {
+    int income();
+    int getAge();
+    boolean identityGiven();
+    boolean proofOfAddress();
+}
+
+class Approval implements UserApprover {
+int salary;
+int age;
+    @Override
+    public int income() {
+        // TODO get Users Income
+        return salary;
+    }
+
+    @Override
+    public int getAge() {
+        // TODO get users age, over 18
+        return age;
+    }
+
+    @Override
+    public boolean identityGiven() {
+        // TODO have they been verified
+        return false;
+    }
+
+    @Override
+    public boolean proofOfAddress() {
+        // TODO valid proof of address
+        return false;
+    }
+    
 }
