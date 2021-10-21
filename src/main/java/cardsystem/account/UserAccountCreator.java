5c889701745e8c04eb49cbf6b0059acb1a850649
emiliarenstroem <21191808@studentmail.ul.ie>
2021-10-21 16:01:21 +0100

    changed UserAccountCreator to class

--- src/main/java/cardsystem/account/UserAccountCreator.java
+++ src/main/java/cardsystem/account/UserAccountCreator.java
@@ -4,11 +4,24 @@ import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.Optional;
 
-public interface UserAccountCreator {
-	public Optional<UserAccount> createNewUserAccount(String name, LocalDate birthDate, BigInteger income);
-	public String createUserId();
-	public UserAccount userLogin();
-	public void closeAccount(String accountId);
+public class UserAccountCreator {
+	public Optional<UserAccount> createNewUserAccount(String name, LocalDate birthDate, BigInteger income) {
+		return Optional.of(new UserAccount(name, birthDate, createUserId(), income));
+	}
+	
+	public String createUserId() {
+		// TODO - implement unique id with database check
+		return "xyz";
+	}
+	
+	public UserAccount userLogin() {
+		// TODO - implement user login
+		return null;
+	}
+	
+	public void closeAccount(String accountId) {
+		// TODO - implement account closure with database
+	}
 }
 
 
