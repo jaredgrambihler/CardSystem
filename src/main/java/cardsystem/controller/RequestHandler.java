a9a0198d3fb0ef97202d75fbff783892f26f9d8e
JaredGrambihler <20299664@studentmail.ul.ie>
2021-10-26 16:53:27 +0100

    Initial implementation of controller structure to handle requests

--- src/main/java/cardsystem/controller/RequestHandler.java
+++ src/main/java/cardsystem/controller/RequestHandler.java
@@ -1,4 +1,27 @@
 package cardsystem.controller;
 
+import com.google.gson.JsonObject;
+
 public class RequestHandler {
+
+    public static JsonObject handleRequest(String action, String requestBody) {
+        switch(action) {
+            case "BalancePayment":
+            case "MerchantTransaction":
+            case "CashAdvanceTransaction":
+            case "CheckBalance":
+            case "GetAvailableCreditLine":
+            case "GetCreditLimit":
+            case "ListTransactions":
+            case "FetchStatementPeriod":
+            case "AccountApplication":
+            case "AccountLogin":
+            case "AccountClosure":
+            case "CreateStatement":
+            default:
+                // Unknown action
+                break;
+        }
+        return new JsonObject();
+    }
 }
