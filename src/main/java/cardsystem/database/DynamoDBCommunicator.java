3eb2f3bdff22c0b8386f880619cfe5e691550443
JaredGrambihler <20299664@studentmail.ul.ie>
2021-10-31 13:17:29 +0000

    Implement DynamoDB configs in database package

---
+++ src/main/java/cardsystem/database/DynamoDBCommunicator.java
@@ -0,0 +1,18 @@
+package cardsystem.database;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+
+import java.util.List;
+
+public class DynamoDBCommunicator implements DatabaseCommunicator {
+
+    @Override
+    public <T> void save(T item) {
+        DynamoDBConfig.getDefaultMapper().save(item);
+    }
+
+    @Override
+    public <T> List<T> query(Class<T> clazz, DynamoDBQueryExpression<T> query) {
+        return DynamoDBConfig.getDefaultMapper().query(clazz, query);
+    }
+}
