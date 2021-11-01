5acaf9354dc04fa8a06ac90c9ccd7fd679aa4bdf
emiliarenstroem <21191808@studentmail.ul.ie>
2021-11-01 15:17:54 +0000

    Refactor: removed interface and updated names in account package

--- src/main/java/cardsystem/account/Account.java
+++ src/main/java/cardsystem/account/Account.java
@@ -1,8 +1,38 @@
 package cardsystem.account;
 
-import java.util.Collection;
+public class Account {
+	private String accountName;
+	private String accountId;
+	private String accountNr;
+	private String userId; 
+	
+	/**
+	 * Create an account
+	 * @param accountName the name of the account
+	 * @param accountId the unique id of the account
+	 * @param accountNr the account number
+	 * @param userId the id of the user who owns the account
+	 */
+	public Account(String accountName, String accountId, String accountNr, String userId) {
+		this.accountName = accountName;
+		this.accountId = accountId;
+		this.accountNr = accountNr;
+		this.userId = userId;
+	}
 
-// Should this be kept?
-public interface Account {
-	public Collection<String> getAccountIds(String userId);
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public String getAccountNr() {
+		return accountNr;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
 }
--- src/main/java/cardsystem/account/AccountCreator.java
+++ src/main/java/cardsystem/account/AccountCreator.java
@@ -3,11 +3,11 @@ package cardsystem.account;
 import java.util.Optional;
 import cardsystem.approval.UserApprover;
 
-public class UserAccountCreator {
+public class AccountCreator {
 	
-	public Optional<UserAccount> createNewUserAccount(String accountName, String accountNr, String userId, int salary, int age, String ssn, String validEmail) {
+	public Optional<Account> createNewAccount(String accountName, String accountNr, String userId, int salary, int age, String ssn, String validEmail) {
 		if (UserApprover.isApproved(salary, age, ssn, validEmail)) {
-			return Optional.of(new UserAccount(accountName, createAccountId(), accountNr, userId));
+			return Optional.of(new Account(accountName, createAccountId(), accountNr, userId));
 		}
 		return Optional.empty();
 	}
