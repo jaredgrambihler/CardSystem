0710f29bac5ec9c9c4f4b853750e737bb59f8ff2
emiliarenstroem <21191808@studentmail.ul.ie>
2021-11-06 16:39:02 +0000

    Added method for converting LocalDate

--- src/main/java/cardsystem/database/DateConverter.java
+++ src/main/java/cardsystem/database/DateConverter.java
@@ -1,6 +1,7 @@
 package cardsystem.database;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -15,5 +16,14 @@ public class DateConverter {
     public static LocalDateTime getLocalDateTime(String iso8601Date) {
         return LocalDateTime.parse(iso8601Date);
     }
+    
+    public static String getIso8601Date(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return date.format(formatter);
+    }
 
+    public static LocalDate getLocalDate(String iso8601Date) {
+        return LocalDate.parse(iso8601Date);
+    }
+    
 }
