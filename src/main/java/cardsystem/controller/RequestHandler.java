7e428e415324fc1bb5464f74bd48d9c2ada9c79f
JaredGrambihler <20299664@studentmail.ul.ie>
2021-11-09 11:56:29 +0000

    Add outline for handling login requests

--- src/main/java/cardsystem/controller/RequestHandler.java
+++ src/main/java/cardsystem/controller/RequestHandler.java
@@ -1,10 +1,17 @@
 package cardsystem.controller;
 
+import cardsystem.models.AccountLogin;
+import cardsystem.models.AccountLoginResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 public class RequestHandler {
 
-    public static JsonObject handleRequest(String action, String requestBody) {
+    private static final Gson gson = new GsonBuilder().create();
+
+    public static Object handleRequest(String action, String requestBody) {
+        Object jsonObject = new JsonObject();
         switch(action) {
             case "BalancePayment":
             case "MerchantTransaction":
@@ -16,12 +23,25 @@ public class RequestHandler {
             case "FetchStatementPeriod":
             case "AccountApplication":
             case "AccountLogin":
+                jsonObject = getAccountLoginResponse(requestBody);
             case "AccountClosure":
             case "CreateStatement":
             default:
                 // Unknown action
                 break;
         }
-        return new JsonObject();
+        return jsonObject;
+    }
+
+    private static AccountLoginResponse getAccountLoginResponse(String requestBody) {
+        AccountLogin accountLogin = gson.fromJson(requestBody, AccountLogin.class);
+        String emailAddress = accountLogin.getEmailAdress();
+        String password = accountLogin.getPassword();
+        // add null check, return some kind of error if a field isn't specified
+        // TODO - get auth token
+        String authToken = "myAuthToken";
+        AccountLoginResponse accountLoginResponse = new AccountLoginResponse();
+        accountLoginResponse.setAuthToken(authToken);
+        return accountLoginResponse;
     }
 }
