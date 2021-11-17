e9f0e1eb0e27f3675a92f80d5196c9253d5a8efe
Nathan Quirke <18248195@studentmail.ul.ie>
2021-11-17 19:50:01 +0000

    updates on TokenFactory and Test Class

--- src/main/java/cardsystem/auth/TokenFactory.java
+++ src/main/java/cardsystem/auth/TokenFactory.java
@@ -1,11 +1,9 @@
 package cardsystem.auth;
 
 import java.util.Base64;
-import java.util.Date;
 import java.util.Optional;
 import io.jsonwebtoken.*;
 import java.security.Key;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import jakarta.xml.bind.DatatypeConverter;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -14,7 +12,8 @@ public class TokenFactory {
 
     /**
      * Get a login token.
-     * @param email users email
+     * 
+     * @param email    users email
      * @param password users password
      * @return a token if the login is valid, an empty optional if it is invalid
      */
@@ -23,44 +22,38 @@ public class TokenFactory {
         return Optional.empty();
     }
 
-
     /**
      * Create a token.
+     * 
      * @param authToken auth token
      * @return token instance
      */
-    
+
     public static Token createToken(String authToken) {
         return null;
     }
 
-    
-    public static final String secretKey= "4C8kum4LxyKWYLM78sKdXrzbBjDCFyfX";
+    public static final String secretKey = "4C8kum4LxyKWYLM78sKdXrzbBjDCFyfX";
 
-    public static String createJWT(String accountId, String issuer, String subject) {
-        
-        //The JWT signature algorithm we will be using to sign the token
+
+    public static String encodeJWT(String accountId, String issuer, String subject) {
+
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-    
-    
-    
-        //We will sign our JWT with our ApiKey secret
+
+
         byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-    
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder().setId(accountId)
-                .setSubject(subject)
-                .setIssuer(issuer)
+
+
+        JwtBuilder builder = Jwts.builder().setId(accountId).setSubject(subject).setIssuer(issuer)
                 .signWith(signatureAlgorithm, signingKey);
-              return builder.compact();
+        return builder.compact();
     }
 
-
-public static Claims decodeJWT(String jwt) {
-    Claims claims = Jwts.parser()
-            .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
-            .parseClaimsJws(jwt).getBody();
-    return claims;
+    
+    public static Claims decodeJWT(String jwt) {
+        Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(secretKey)).parseClaimsJws(jwt)
+                .getBody();
+        return claims;
     }
 }
--- src/test/java/cardsystem/auth/TestToken.java
+++ src/test/java/cardsystem/auth/TestToken.java
@@ -1,5 +1,41 @@
 package cardsystem.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import org.junit.Test;
+import org.apache.commons.logging.impl.Log4JLogger;
+import org.apache.commons.logging.*;
+import static org.junit.Assert.*;
+import java.util.logging.LogManager;
+
 public class TestToken {
-    
+
+    private static final Log4JLogger logger = LogManager.logger();
+
+
+    @Test
+    public void createDecodeJWT() {
+
+        String accountId = "Nathan Quirke";
+        String issuer = "JWT Demo";
+        String subject = "Nathan";
+
+        String jwt = TokenFactory.encodeJWT(accountId, issuer, subject)
+                jwtAccountId, 
+                jwtIssuer, 
+                jwtSubject, 
+        );
+        
+        logger.info("jwt = \"" + jwt.toString() + "\"");
+
+        Claims claims = TokenFactory.decodeJWT(jwt);
+
+        logger.info("claims = " + claims.toString());
+
+        assertEquals(jwtAccountId, claims.getId());
+        assertEquals(jwtIssuer, claims.getIssuer());
+        assertEquals(jwtSubject, claims.getSubject());
+
+    }
 }
