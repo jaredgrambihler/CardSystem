0fa790fc032de2b1a9e6997dbb10d09d43aa5115
JaredGrambihler <20299664@studentmail.ul.ie>
2021-11-17 20:21:40 +0000

    Fix compile errors

--- src/test/java/cardsystem/auth/TestToken.java
+++ src/test/java/cardsystem/auth/TestToken.java
@@ -1,18 +1,10 @@
 package cardsystem.auth;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureException;
 import org.junit.Test;
-import org.apache.commons.logging.impl.Log4JLogger;
-import org.apache.commons.logging.*;
 import static org.junit.Assert.*;
-import java.util.logging.LogManager;
 
 public class TestToken {
-
-    private static final Log4JLogger logger = LogManager.logger();
-
 
     @Test
     public void createDecodeJWT() {
@@ -21,21 +13,15 @@ public class TestToken {
         String issuer = "JWT Demo";
         String subject = "Nathan";
 
-        String jwt = TokenFactory.encodeJWT(accountId, issuer, subject)
-                jwtAccountId, 
-                jwtIssuer, 
-                jwtSubject, 
+        String jwt = TokenFactory.encodeJWT(
+                accountId,
+                issuer,
+                subject
         );
-        
-        logger.info("jwt = \"" + jwt.toString() + "\"");
-
         Claims claims = TokenFactory.decodeJWT(jwt);
-
-        logger.info("claims = " + claims.toString());
-
-        assertEquals(jwtAccountId, claims.getId());
-        assertEquals(jwtIssuer, claims.getIssuer());
-        assertEquals(jwtSubject, claims.getSubject());
+        assertEquals(accountId, claims.getId());
+        assertEquals(issuer, claims.getIssuer());
+        assertEquals(subject, claims.getSubject());
 
     }
 }
