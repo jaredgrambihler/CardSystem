5bef2c31d0c436558b9ec67f06a7a545cc2e1fe2
Nathan Quirke <18248195@studentmail.ul.ie>
2021-11-17 07:04:03 +0000

    update to TokenFactory to implement authToken

--- src/main/java/cardsystem/auth/TokenFactory.java
+++ src/main/java/cardsystem/auth/TokenFactory.java
@@ -1,9 +1,14 @@
 package cardsystem.auth;
 
 import java.util.Base64;
+import java.util.Date;
 import java.util.Optional;
-
-
+import io.jsonwebtoken.*;
+import java.security.Key;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import jakarta.xml.bind.DatatypeConverter;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class TokenFactory {
 
@@ -13,30 +18,49 @@ public class TokenFactory {
      * @param password users password
      * @return a token if the login is valid, an empty optional if it is invalid
      */
+
     public static Optional<Token> getLoginToken(String email, String password) {
         return Optional.empty();
     }
 
-    public static Token createToken(String authToken) {
-        return null;
-    }
 
     /**
      * Create a token.
      * @param authToken auth token
      * @return token instance
      */
+    
+    public static Token createToken(String authToken) {
+        return null;
+    }
 
-public static String encode(String authToken) {
+    
+    public static final String secretKey= "4C8kum4LxyKWYLM78sKdXrzbBjDCFyfX";
 
-    return Base64.getEncoder().encodeToString(authToken.getBytes());
+    public static String createJWT(String accountId, String issuer, String subject) {
+        
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    
+    
+    
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+    
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(accountId)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+              return builder.compact();
+    }
 
-}
 
-public static String decode(String authToken) {
-
-    byte[] decodedBytes = Base64.getDecoder().decode(authToken);
-    return new String(decodedBytes);
-
+public static Claims decodeJWT(String jwt) {
+    Claims claims = Jwts.parser()
+            .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
+            .parseClaimsJws(jwt).getBody();
+    return claims;
     }
 }
